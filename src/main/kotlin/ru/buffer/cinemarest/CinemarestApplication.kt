03d061cf63702154411a29e537edd62df43cf3d9
buffer112 <gogi-pankraxin@mail.ru>
2021-03-12 11:24:40 +0300

    Add project in git

---
+++ src/main/kotlin/ru/buffer/cinemarest/CinemarestApplication.kt
@@ -0,0 +1,11 @@
+package ru.buffer.cinemarest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CinemarestApplication
+
+fun main(args: Array<String>) {
+    runApplication<CinemarestApplication>(*args)
+}
---
+++ src/test/kotlin/ru/buffer/cinemarest/CinemarestApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.buffer.cinemarest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CinemarestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
